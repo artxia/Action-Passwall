4f059c59a1840496a605d27e8b11782f084c27e8
xia <50513568+artxia@users.noreply.github.com>
2020-12-03 01:22:50 +0800

    Update custom.sh

--- user/common/custom.sh
+++ user/common/custom.sh
@@ -2,6 +2,6 @@
 
 echo "Apply custom.sh"
 
-git clone https://github.com/xiaorouji/openwrt-package.git -b master package/openwrt-package               
+git clone https://github.com/xiaorouji/openwrt-passwall.git -b master package/openwrt-package               
 
 
